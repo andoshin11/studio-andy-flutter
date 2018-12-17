6f5ebc8d24eb96ad1d0fddabefc01a27d8425cd0
andoshin11 <shinglish11@gmail.com>
2018-12-18 01:45:21 +0900

    Add getAssets method

--- lib/gateways/contentful_gateway.dart
+++ lib/gateways/contentful_gateway.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:studio_andy_app/infra/api_client.dart';
 import 'package:studio_andy_app/infra/api_request.dart';
 import 'package:studio_andy_app/entities/post.dart';
+import 'package:studio_andy_app/entities/asset.dart';
 
 class ContentfulGateway {
   Future<List<Post>> getPosts() async {
@@ -20,5 +21,17 @@ class ContentfulGateway {
         res['items'].map((item) => Post.fromJson(item['fields'])).cast<Post>();
 
     return posts.toList();
+  }
+
+  Future<List<Asset>> getAssets() async {
+    var res = await APIClient.request(new APIRequest(
+        path:
+            '/spaces/${DotEnv().env['CTF_SPACE_ID']}/environments/master/assets',
+        params: {'access_token': DotEnv().env['CTF_CDA_ACCESS_TOKEN']},
+        method: HTTPMethod.GET));
+
+    var assets =
+        res['items'].map((item) => Asset.fromJson(item)).cast<Asset>().toList();
+    return assets;
   }
 }
